6c2bccef275d2e42063c1ea1c5b58ed1d0124dd7
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-01-22 20:16:07 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Arlodragon",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "254702713600",              
+    OWNER_NAME: process.env.OWNER_NAME || "MSNAPDRAGON",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254704009677",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_REACT_STATUS : process.env.AUTO_REACT_STATUS || 'yes',
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
